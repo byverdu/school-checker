6c08db3723e36ae50db88139e8e5240e425af32f
byverdu <byverdu@gmail.com>
2019-07-16 21:59:58 +0100

    added more props to scrape from cypress

--- scrapper/cypress/integration/scrapper.spec.js
+++ scrapper/cypress/integration/scrapper.spec.js
@@ -8,7 +8,7 @@ const {
 } = require('../../utils');
 
 const data = [];
-const props = {
+const schoolProps = {
   address: 'Address:',
   type: 'School type:',
   sex: 'Gender of entry:',
@@ -18,19 +18,24 @@ const props = {
   extraInfo: 'Further information:',
   ofsted: 'Ofsted rating:'
 };
-const test = schools.slice(0, 3);
+const schoolStats = {
+  statsReading: 'Reading',
+  statsWriting: 'Writing',
+  statsMaths: 'Maths'
+}
+const test = schools.slice(0, 1);
 
 describe('Name of the group', () => {
   after(() => {
     cy.writeFile('config/schools-data.json', JSON.stringify(data));
   });
   
-  schools.forEach(school => {
+  schools.forEach(schoolURLReport => {
     const id = uuid();
     let schoolsMapper = {};
     describe('School Scrapper', function() {
       before(() => {
-        cy.visit(school);
+        cy.visit(schoolURLReport);
       });
       after(() => {
         data.push(schoolsMapper);
@@ -44,21 +49,21 @@ describe('Name of the group', () => {
             schoolsMapper = {
               ...schoolsMapper,
               id,
-              name
+              name,
+              schoolURLReport
             }
 
             expect(true).to.equal(true)
           });
         });
-      
-        
-      Object.keys(props).forEach(key => {
-        it(`Gets ${props[key]} value`, () => {
+
+      Object.keys(schoolProps).forEach(key => {
+        it(`Gets ${schoolProps[key]} value`, () => {
           cy.get('dl.metadata-school-detail')
-            .contains(`${props[key]}`)
+            .contains(`${schoolProps[key]}`)
             .next('dd')
             .then((element) => {
-              console.log(element)
+
               let value = element[0].textContent;
 
               if (key === 'website' || key === 'extraInfo') {
@@ -96,6 +101,81 @@ describe('Name of the group', () => {
 
               expect(true).to.equal(true)
             });
+          });
+      });
+
+      Object.keys(schoolStats).forEach((key, index) => {
+        it(`Gets stats for ${schoolStats[key]}`, () => {
+          cy.get('.primary-school-progress')
+            .find('.column-third')
+            .eq(index)
+            .find('.scores')
+            .then(element => {
+
+              const text = element[0].querySelector('.label-label').textContent.trim();
+              const score = element[0].querySelector('.label-value').textContent.trim();
+
+              schoolsMapper = {
+                ...schoolsMapper,
+                [`${key}`]: `${text}:${score}`
+              }
+            });
+        });
+      });
+
+      it('Gets the info for some metrics', () => {
+        cy.get('.data-widget')
+          .then(elements => {
+
+            function getSvgElement (element) {
+              return element.querySelector('svg').outerHTML;
+            }
+
+            function getScoreText (element, index) {
+              return element.querySelectorAll('text')[index].textContent.replace('%', '');
+            }
+
+            schoolsMapper = {
+              ...schoolsMapper,
+              charts: {
+                expected: {
+                  text: 'Meeting expected Standard',
+                  svg: getSvgElement(elements[0]),
+                  score: {
+                    school: getScoreText(elements[0], 0),
+                    localAuthoroty: getScoreText(elements[0], 1),
+                    england: getScoreText(elements[0], 2)
+                  }
+                },
+                higher: {
+                  text: 'Achieving higher Standard',
+                  svg: getSvgElement(elements[1]),
+                  score: {
+                    school: getScoreText(elements[1], 0),
+                    localAuthority: getScoreText(elements[1], 1),
+                    england: getScoreText(elements[1], 2)
+                  }
+                },
+                readScore: {
+                  text: 'Average score in reading',
+                  svg: getSvgElement(elements[2]),
+                  score: {
+                    school: getScoreText(elements[2], 0),
+                    localAuthority: getScoreText(elements[2], 1),
+                    england: getScoreText(elements[2], 2)
+                  }
+                },
+                writeScore: {
+                  text: 'Average score in writing',
+                  svg: getSvgElement(elements[3]),
+                  score: {
+                    school: getScoreText(elements[3], 0),
+                    localAuthority: getScoreText(elements[3], 1),
+                    england: getScoreText(elements[3], 2)
+                  }
+                },
+              }
+            }
           });
       });
     });
