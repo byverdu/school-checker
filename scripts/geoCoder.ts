1ca3441a9a8ce4a9572479c65e44e6fcf643b345
byverdu <byverdu@gmail.com>
2019-08-06 20:47:39 +0100

    added geoData to schools data

---
+++ scripts/geoCoder.ts
@@ -0,0 +1,68 @@
+import { School } from '../src/models';
+const schoolsData = require('../shared-data/schools-data.json');
+
+window.localStorage.setItem('geoLocations', JSON.stringify([]));
+
+(window as any).initMap = () => {
+  var position;
+  var map;
+  var delay = 100;
+  var geo = new google.maps.Geocoder();
+  var nextAddress = 0;
+  // var geodecoder
+  map = new google.maps.Map(
+    document.getElementById('map'),
+    { center: new google.maps.LatLng(51.458509, -0.2058498999999756), zoom: 16 });
+
+  function getAddress(school: School, next) {
+    geo.geocode({ address: school.postcode }, function (results, status) {
+      // If that was successful
+      if (status == google.maps.GeocoderStatus.OK) {
+        // Lets assume that the first marker is the one we want
+        const geoLocations = JSON.parse(window.localStorage.getItem('geoLocations'));
+
+        geoLocations.push({
+          name: school.name,
+          id: school.id,
+          lat: results[0].geometry.location.lat(),
+          lng: results[0].geometry.location.lng()
+        });
+
+        window.localStorage.setItem('geoLocations', JSON.stringify(geoLocations));
+      }
+      // ====== Decode the error status ======
+      else {
+        // === if we were sending the requests to fast, try this one again and increase the delay
+        if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+          nextAddress--;
+          delay++;
+          console.log('OVER_QUERY_LIMIT', localStorage)
+        } else {
+          const geoLocations = JSON.parse(window.localStorage.getItem('geoLocations'));
+
+          geoLocations.push({
+            name: school.name,
+            id: school.id,
+            lat: results[0].geometry.location.lat(),
+            lng: results[0].geometry.location.lng()
+          });
+
+          window.localStorage.setItem('geoLocations', JSON.stringify(geoLocations));
+        }
+      }
+      next();
+    }
+    );
+  }
+
+  function theNext() {
+    if (nextAddress < schoolsData.length) {
+      setTimeout(() => { getAddress(schoolsData[nextAddress], theNext) }, delay);
+      nextAddress++;
+    } else {
+      console.log(localStorage)
+    }
+  }
+
+  theNext();
+}
--- scripts/mergeJsonData.js
+++ scripts/mergeJsonData.js
@@ -1,23 +1,50 @@
 const admissionData = require('../shared-data/schools-data-admission.json');
 const schoolsData = require('../shared-data/schools-data-web.json');
+const geocodeData = require('../shared-data/schools-geocodes.json');
 const fs = require('fs');
 
-const tempData = schoolsData.map(element => {
-  const {admissionNumber, furthestDistance} = admissionData.find(item => item.name === element.name);
+let geoTempData;
 
-  return {
-    ...element,
-    admissionNumber,
-    furthestDistance
-  };
-});
+try {
+    geoTempData = schoolsData.map(element => {
+    const geo = geocodeData.find(item => item.id === element.id);
+  
+    if (geo) {
+      return {
+        ...element,
+        lat: geo.lat,
+        lng: geo.lng
+      };
+    }
+  
+    return null;
+  }).filter(notNull => notNull !== null);
+  
+} catch (e) {
+  throw e.message;
+}
 
-console.log(tempData[0]);
+try {
+  const adminTempData = geoTempData.map(element => {
+    const {admissionNumber, furthestDistance} = admissionData.find(item => item.name === element.name);
+  
+    return {
+      ...element,
+      admissionNumber,
+      furthestDistance
+    };
+  });
+  
+  console.log(adminTempData)
+  
+  fs.writeFile(
+    'shared-data/schools-data.json',
+    JSON.stringify(adminTempData),
+    (err) => {
+      if (err) console.log(err)
+    }
+  );
 
-fs.writeFile(
-  'shared-data/schools-data.json',
-  JSON.stringify(tempData),
-  (err) => {
-    if (err) console.log(err)
-  }
-);
+} catch (e) {
+  throw e.message;
+}
