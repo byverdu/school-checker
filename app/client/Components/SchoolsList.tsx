41a6cac9731059f1bd7c122fe8ed0ef4f55e9ad7
byverdu <byverdu@gmail.com>
2019-09-26 00:15:41 +0100

    added filter schools by name

--- app/client/Components/SchoolsList.tsx
+++ app/client/Components/SchoolsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, memo } from 'react';
 import { Link } from 'react-router-dom';
 import { School } from 'Models/School';
 
@@ -7,24 +7,38 @@ interface SchoolsListProps {
   activeId: any;
 }
 
-const SchoolsList: React.SFC<SchoolsListProps> = ({ schools, activeId }) => (
-  <div className="school-link-container">
-    {
-      schools.map(school => {
-        const activeClass = activeId === school.id ? 'school-link-active' : null;
+const SchoolsList: React.SFC<SchoolsListProps> = ({ schools, activeId }) => {
 
-        return (
-          <Link
-            key={school.id}
-            to={`/school/${school.id}`}
-            className={`school-link ${activeClass}`}
-          >
-            {school.name}
-          </Link>
-        )
-      })
-    }
-  </div>
-);
+  const [filteredSchools, setFilteredSchools] = useState(schools);
+  const onChangeHandler = (e: React.ChangeEvent) => {
+    const value = (e.target as HTMLInputElement).value.toLowerCase();
+    const newSchoolsSet = schools.filter(school => school.name.toLowerCase().includes(value));
+
+    setFilteredSchools(newSchoolsSet);
+  } 
+    
+  return (
+    <>
+      <input className="school-link-filter" type="text" placeholder="Filter by name" onChange={onChangeHandler}/>
+      <div className="school-link-container">
+        {
+          filteredSchools.map(school => {
+            const activeClass = activeId === school.id ? 'school-link-active' : '';
+    
+            return (
+              <Link
+                key={school.id}
+                to={`/school/${school.id}`}
+                className={`school-link ${activeClass}`}
+              >
+                {school.name}
+              </Link>
+            )
+          })
+        }
+      </div>
+    </>
+  );
+}
 
 export default SchoolsList;
