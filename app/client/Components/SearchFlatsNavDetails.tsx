485fc0d942a21ccd33c70ce1f488aaf6b27c2e3d
byverdu <byverdu@gmail.com>
2019-09-29 17:24:43 +0100

    styles for search flats form

--- app/client/Components/SearchFlatsNavDetails.tsx
+++ app/client/Components/SearchFlatsNavDetails.tsx
@@ -2,9 +2,12 @@ import React, { SFC } from 'react';
 import { formInputs } from 'config';
 
 const FlatsForm: React.SFC<{onSubmit: (e) => void}> = ({onSubmit}) => (
-  <form noValidate onSubmit={onSubmit}>
-    {formInputs.map(input => <input {...input} key={input.name} />)}
-    <input type="submit" className="xoxo" />
+  <form noValidate onSubmit={onSubmit} className="flats-search-form">
+    {formInputs.map(input => <label className="flats-search-label"  key={input.name}>
+      <span>{input.name}</span>
+      <input className="flats-search-input" {...input} />
+    </label>)}
+    <input type="submit" className="flats-search-input" />
   </form>
 );
 
